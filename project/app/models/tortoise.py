32bf95f81544c90099a813da9e8a29e34f5ebb8d
Darko Kolev <kolevdarko@gmail.com>
2022-10-01 12:10:22 +0200

    tortoise working

--- project/app/models/tortoise.py
+++ project/app/models/tortoise.py
@@ -2,4 +2,9 @@ from tortoise import fields, models
 
 
 class TextSummary(models.Model):
-    url = fields.Te
+    url = fields.TextField()
+    summary = fields.TextField()
+    created_at = fields.DatetimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.url
